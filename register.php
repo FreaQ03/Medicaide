dc0b145b63e474849ad4142ebd27a3d64fdfd199
Frosco <origamer038@gmail.com>
2021-05-24 16:37:59 +0800

    Registration update
    Added feature to make sure email is unique

--- register.php
+++ register.php
@@ -25,36 +25,29 @@
 <body>
 
 	<!--Navbar-->
-	<nav class="navbar navbar-expand-lg navbar-light sticky-top" id="navbar-color">
+  <nav class="navbar navbar-expand-lg navbar-light sticky-top" id="navbar-color">
   <div class="container-fluid sticky-top">
-    <a class="navbar-brand" href="#">Medicaide</a>
+    <a class="navbar-brand" href="#" style="color: #A4292E;"><b>MEDICAIDE</b></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+        </ul>
+      <div class="nav-links">
+        <ul>
+          <a href="#"><li>ABOUT US</li></a>
+          <a href="#"><li>CONTACT US</li></a>
+          <a href="#"><li>PRIVACY POLICY</li></a>
+        </ul>
+      </div>   
+          <a class="nav-link icon" id="calendar" aria-current="page" href="#" style="color: #A4292E;"> <i class="fas fa-calendar-plus"></i></a>
+          <a class="nav-link icon" id="phone" aria-current="page" href="#" style="color: #A4292E;"> <i class="fas fa-phone-square-alt"></i></a>
+          <a class="nav-link icon" id="clock" aria-current="page" href="#" style="color: #A4292E;">  <i class="fas fa-clock"></i></a>
+          <a class="nav-link icon" id="prescriptions" aria-current="page" href="#" style="color: #A4292E;"> <i class="fas fa-notes-medical"></i></a>
+          <a class="nav-link icon" id="journal" aria-current="page" href="#" style="color: #A4292E;"> <i class="fas fa-sticky-note"></i></a>
+          <a class="nav-link icon" id="user" aria-current="page" href="#" style="color: #A4292E;"> <i class="fas fa-user"></i></a>
+        </li> 
     </div>
   </div>
 </nav>
@@ -126,6 +119,19 @@
                 <button type="submit" class="btn btn-primary subheading">Sign Up</button>
               </div>
           </form>
+          <?php
+            if (isset($_GET['origemail'])) { //check if authenticate key exists in URL
+              if ($_GET['origemail'] == "false") {
+                echo '
+                  <br>
+                  <div class="alert alert-warning ml-5 mr-5" role="alert">
+                  This email already exists, please use another email.
+                  </div>
+                ';
+              }
+            }
+          ?>
+
         </div>
       </center>
     </div>
