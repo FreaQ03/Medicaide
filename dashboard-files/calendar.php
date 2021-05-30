bc5f6c31f80b654015a811160f7552a3288ac5e6
ayaHiraya <84461408+ayaHiraya@users.noreply.github.com>
2021-05-30 17:46:22 +0800

    updates updates

--- dashboard-files/calendar.php
+++ dashboard-files/calendar.php
@@ -1,1 +1,39 @@
-<h1>I am a calendar :D</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>calendar</title>
+	<link rel="stylesheet" type="text/css" href="calendar.css">
+	<link rel="stylesheet" type="text/css" href="evo-calendar.min.css">
+  	<link rel="stylesheet" type="text/css" href="evo-calendar.midnight-blue.min.css">
+</head>
+<body>
+	<div class="hero">
+		<div id="calendar">
+			
+
+
+
+
+
+
+		</div>
+	</div>
+	<!-- Add jQuery library (required) -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+	<script src="evo-calendar.min.js"></script>
+
+
+	<script> 
+				$(document).ready(function() {
+		    $('#calendar').evoCalendar({
+		    })
+		})
+	</script>
+
+
+
+
+
+
+</body>
+</html>
