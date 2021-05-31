2d43decd091883516a213a2e37824f2ed62c5947
ayaHiraya <84461408+ayaHiraya@users.noreply.github.com>
2021-05-31 15:57:19 +0800

    journal updates

--- dashboard-files/journal.php
+++ dashboard-files/journal.php
@@ -1,19 +1,37 @@
-<html>
+<doctype html>
+
 <head>
-    <title></title>
+
+	<!--Boostrap CSS-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+	<!--Fontawesome-->
+	<script src="https://kit.fontawesome.com/6af8a38aa6.js" crossorigin="anonymous"></script>
+
+	<!--Animate CSS-->
+	<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+
+	<!--Google Fonts API-->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
 
 
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<link href='https://fonts.googleapis.com/css?family=Roboto:400,300italic,300,100italic,100,400italic,500,500italic,700' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css" href="css/journal.css">
+  
 </head>
+
+
+
+
+
 <body>
 
+
     <div id = "journal">
-          <div class = "photo">
-                <img src = "http://placehold.it/140x100" id = "userImage">
-                <button id = "upload">Upload</button>
-          </div>
+         
           <div class = "comments">
                 <textarea id = "content" cols = "64" rows = "3" placeholder = "Say something about yourself."></textarea>
                 <button id = "submit" onclick = "isEmpty();clearDiv();">Submit</button>
@@ -33,4 +51,5 @@
 
 
 </body>
+
 </html>
