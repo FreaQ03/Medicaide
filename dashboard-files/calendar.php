9a99d6d015b3eca2962da8ca3cded2453f8bb325
Frosco <origamer038@gmail.com>
2021-06-02 15:28:33 +0800

    Small update for calendar

--- dashboard-files/calendar.php
+++ dashboard-files/calendar.php
@@ -1,5 +1,6 @@
 <!--<div class="responsiveCalendar">
 	<iframe src="dashboard-files/iframe/JSCalendar.php" id="calendarFrame" scrolling="no"></iframe>
-</div>-->
+</div>
+-->
 
-<h1>This is the calendar page</h1>
+<h1> I am a calendar</h1>
