82813e0b9c747b8956904a6b26ad327ec6a34c84
Frosco <origamer038@gmail.com>
2021-06-06 00:38:05 +0800

    Added back-end for journal submit

--- dashboard-files/journal.php
+++ dashboard-files/journal.php
@@ -3,30 +3,35 @@
 		<h1>Journal Entries</h1>	          
 	</div>
 
-		<div class = "journalEntry">
-	  		<form name="controls" id="controls">
-	 <!--dropdown for days--------------->
-	 		<div class="dropdown">
-			  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			    Dropdown button
-			  </button>
-			  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-			    <a class="dropdown-item" href="#">Action</a>
-			    <a class="dropdown-item" href="#">Another action</a>
-			    <a class="dropdown-item" href="#">Something else here</a>
-			  </div>
+	<div class = "journalEntry">
+  		<form name="controls" id="controls" action="functions/submitJournal.php" method="post">
+
+ 		<!--dropdown for days-->
+ 		<div class="dropdown">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Dropdown button
+			</button>
+			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			<a class="dropdown-item" href="#">Action</a>
+			<a class="dropdown-item" href="#">Another action</a>
+			<a class="dropdown-item" href="#">Something else here</a>
 			</div>
-	 <!--Initial Journal Entry--------------->
-	<div class="journalOutput">
-	    <br/><textarea id="output" name="output" rows="9" wrap="virtual" cols=48 placeholder="Write your day here..." ></textarea><br>
-	    <ul><p align="center"><input type="reset" value="clear"></p></ul>
-	    
-	  </form>
+		</div>
+	<!--Initial Journal Entry-->
+		<div class="journalOutput">
+		    <br/><textarea id="output" name="journalData" rows="9" wrap="virtual" cols=48 placeholder="Write your day here..." ></textarea><br>
+		    <ul><p align="center"><input type="reset" value="clear"></p></ul>
+		  	
+		</div>
+		<div class="text-center">
+		    <input class="btn btn-outline-success btn-lg" type="submit" value="submit">
+		</div>
+
+		</form>
 	</div>
 </div>
-<div class="text-center">
-    <a class="btn btn-outline-success btn-lg" href="#" role="button">Submit</a>  
-</div>
+
+
   
 
     
---
+++ functions/submitJournal.php
@@ -0,0 +1,46 @@
+<?php
+	session_start();
+
+	//Set variables
+	$formData = $_POST['journalData'];
+	$patientID = $_SESSION['userID'];
+	$date = date("Y-m-d");
+
+	//1. Setup Database connection
+	$servername = "localhost";
+	$db_username = "root"; //xampp default
+	$db_password = "";  //xampp default
+	$database = "medicaide";
+
+	$conn = mysqli_connect($servername, $db_username, $db_password, $database);
+
+	//I. Find Journal ID of Patient
+
+	//1.1 Select SQL
+	$journalID_Query = "SELECT * FROM `journal` WHERE `pat_id`=" . $patientID; 
+	
+	//1.2 Execute SQL
+	$result = mysqli_query($conn, $journalID_Query);
+	$row = mysqli_fetch_assoc($result);
+
+	//1.3 Set journal ID
+	$journalID = $row["id"];
+
+	//II. Inserting data to database
+
+	//2.1 SELECT SQL
+	$sql = 'INSERT INTO `journal_entries` (`pat_id`, `journal_id`, `data`, `createdOn`) VALUES (' . $patientID . ', ' . $journalID . ', "' . $formData . '", "' . $date . '")';
+
+	//3. Execute SQL
+	if(mysqli_query($conn, $sql)) {
+		header('Location: ../dashboard.php');
+	}
+
+	else {
+		echo mysqli_error($conn);
+	}
+
+	//4. Closing Database Connection
+  	mysqli_close($conn);
+
+?>
