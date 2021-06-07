3e9224336ea45d23b26ac3b2194b9a936ba6b99f
Frosco <origamer038@gmail.com>
2021-06-07 16:23:20 +0800

    Updated bootstrap css and JS

--- dashboard-files/search.php
+++ dashboard-files/search.php
@@ -55,3 +55,4 @@
 	<iframe class="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7724.218866917946!2d121.0139724976897!3d14.535732494615596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c93a386baf7f%3A0xba1f27fb436114e3!2sMagallanes%2C%20Makati%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1622886943473!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 	
 </div>
+</div>
