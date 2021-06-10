494824967715a1dc5d497c60fe36266608e01639
Frosco <origamer038@gmail.com>
2021-06-10 23:28:47 +0800

    Added connection file for xampp and mamp compatibility

---
+++ functions/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+  //1. Setup Database connection
+  $servername = "localhost";
+  $db_username = "root"; //xampp default
+  $db_password = "";  //xampp default
+  $database = "medicaide";
+
+  $conn = mysqli_connect($servername, $db_username, $db_password, $database);
+
+?>
