79953e1dd38fa020058fbaf60364372d2119a26b
Frosco <origamer038@gmail.com>
2021-06-11 00:12:25 +0800

    Changed JQUERY cdn

--- dashboardPatients.php
+++ dashboardPatients.php
@@ -3,8 +3,6 @@
 
 	<!--Boostrap CSS-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <!--Ajax-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
   <!--w3schools-->
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
