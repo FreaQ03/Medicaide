fac5d182e3b3a54e22602498244e3a30f1069405
APldl <adrianlopezdeleon@gmail.com>
2021-06-12 20:49:22 +0800

    toast wont show

--- dashboardPatients.php
+++ dashboardPatients.php
@@ -55,6 +55,52 @@
     </div>
     </div>
   </nav>
+
+<!--TOAST-->
+
+<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+  <!-- Position it -->
+  <div style="position: absolute; top: 0; right: 0;">
+
+    <!-- Then put toasts within -->
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <img src="..." class="rounded mr-2" alt="...">
+        <strong class="mr-auto">Bootstrap</strong>
+        <small class="text-muted">just now</small>
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="toast-body">
+        See? Just like this.
+      </div>
+    </div>
+
+      <!-- pag nadagdagan (here) automatic sila stacked -->
+
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!--Sidebar Functions-->
 <div class="container m-0 p-0 d-inline" id="dynamicBody">
 
