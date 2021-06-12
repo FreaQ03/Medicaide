31bf6dd8f23e616c4d70398a728b51f9573011b1
APldl <adrianlopezdeleon@gmail.com>
2021-06-12 23:22:56 +0800

    doctor dashboard toast

--- dashboardDoctors.php
+++ dashboardDoctors.php
@@ -97,12 +97,17 @@
           <strong class="mr-auto">Name Here</strong>
         </div>
         <div class="toast-body">
-          Your appointment has been accepted!
+          (name) wants to book an appointment at (time) (date) (idk yung content)
         </div>
-        <button type="button" class="dismissbtn btn-primary" data-dismiss="toast" >
-            DISMISS
+        <div class="doc_choice">
+        <button type="button" class="dismissbtn btn-success" data-dismiss="toast" >
+            ACCEPT
           </button>
+        <button type="button" class="dismissbtn btn-danger" data-dismiss="toast" >
+            DECLINE
+        </button>
       </div>
+    </div>
 
           <!-- When auto added marami na notif, stacked sila automatically -->
 
@@ -114,24 +119,39 @@
           <strong class="mr-auto">Name Here</strong>
         </div>
         <div class="toast-body">
-          Your appointment has been accepted!
+          (name) wants to book an appointment at (time) (date) (idk yung content)
         </div>
-        <button type="button" class="dismissbtn btn-primary" data-dismiss="toast" >
-            DISMISS
+        <div class="doc_choice">
+        <button type="button" class="dismissbtn btn-success" data-dismiss="toast" >
+            ACCEPT
           </button>
+        <button type="button" class="dismissbtn btn-danger" data-dismiss="toast" >
+            DECLINE
+        </button>
       </div>
+    </div>
           
            -->
 
 
+
+
+
+
     </div>
   </div>
+</div>
 </div>
 
   <!--Bootstrap Javascript-->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+  <script>
+    $('.toast').toast('show');
+  </script>
 
   <!--Full Jquery for other functions-->
   <script
