6832e2ba1598d0e4477f6e6a0e346c6267ad796a
Frosco <origamer038@gmail.com>
2021-06-16 20:44:07 +0800

    verify modal

--- dashboardDoctors.php
+++ dashboardDoctors.php
@@ -141,12 +141,41 @@
     </div>
   </div>
 </div>
+
+
+<!-- Verification Modal -->
+<div class="modal fade" id="verifyModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title verify-title" id="exampleModalLabel">Verification form sent!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Verification form sent please wait 1-2 business days for us to verify your information.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger modal-close" data-dismiss="modal">CLOSE</button>
+      </div>
+    </div>
+  </div>
 </div>
+
 
   <!--Bootstrap Javascript-->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <?php 
+    //Show verification modal after submitting verification form
+    if(isset($_GET['verifySent'])){
+      echo "<script> $('#verifyModal').modal('show');</script>";
+    } 
+
+  ?>
 
 
   <script>
