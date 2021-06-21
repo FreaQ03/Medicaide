dcb9d399e216bf1385d62db940f5b6896272ff6c
Frosco <origamer038@gmail.com>
2021-06-22 01:13:33 +0800

    edit schedule modal

--- dashboardDoctors.php
+++ dashboardDoctors.php
@@ -204,6 +204,27 @@
   </div>
 </div>
 
+<!-- Edit Hospital Modal -->
+<div class="modal fade" id="editSuccessModal" data-backdrop="static" data-keyboard="false"  tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Hospital Details Updated</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Your hospital and schedule details have been successfully updated!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">DISMISS</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
   <!--Bootstrap Javascript-->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -214,6 +235,13 @@
     //Show verification modal after submitting verification form
     if(isset($_GET['verifySent'])){
       echo "<script> $('#verifyModal').modal('show');</script>";
+    } 
+    //Show verification modal after submitting verification form
+    if(isset($_GET['editSuccess'])){
+      if($_GET['editSuccess'] == 1){
+        echo "<script> $('#editSuccessModal').modal('show');</script>";
+      }
+      
     } 
 
     //Show verification notification modal if user is not yet verified
--- doc_editSched.php
+++ doc_editSched.php
@@ -172,37 +172,15 @@
 
               <button type="button" id="addSched" class="btn btn-success">Add Schedule</button>
               <input type="submit" value="Submit" >
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-              </button>
 
       <div class="register_link mt-2"> 
          <a href="login.php"></a>
       </div>
       </div>
 
-      </div>
-      </div>
+      
     </form>
 
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
 
     <script>
       
